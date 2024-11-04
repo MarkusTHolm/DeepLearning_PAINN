af2e8d2a5e7bf6f7f190ca6871ef19a769db973a
MarkusTHolm <mtaho@dtu.dk>
2024-11-04 16:42:54 +0100

    First version of pseudo implementation is done

--- src/models/painn.py
+++ src/models/painn.py
@@ -6,7 +6,6 @@ class PaiNN(nn.Module):
     """
     Polarizable Atom Interaction Neural Network with PyTorch.
     
-    This is Sebastian
     """
     def __init__(
         self,
@@ -33,6 +32,9 @@ class PaiNN(nn.Module):
         """
         super().__init__()
 
+        self.cutoff_dist = cutoff_dist
+        self.num_message_passing_layers = num_message_passing_layers
+
     def forward(
         self,
         atoms: torch.LongTensor,
@@ -57,14 +59,76 @@ class PaiNN(nn.Module):
         Returns:
             A torch.FloatTensor of size [num_nodes, num_outputs] with atomic
             contributions to the overall molecular property prediction.
+
         """
-        raise NotImplementedError
+
+        N = len(atoms)
+
+        # Make adjecency matrix
+        A_row, A_col = self.make_adjecency_matrix(atom_positions, graph_indexes)
+
+        # Find relative positions
+        r_ij = atom_positions[A_col] - atom_positions[A_row]
+
+        # Get Z and embeddings
+        Z = atoms[A_row]  #TODO: check this
+        s_i0 = nn.Embedding()
+
+        v_i = torch.zeros((self.num_features, N, 3)) # TODO: check sizes
+
+        # Perform message passing
+        for i in range(self.num_message_passing_layers):
+            delta_vi, delta_si = Message(v_j, s_j, r_ij)
+            v_j = delta_vi + v_j
+            s_j = delta_si + s_j
+            delta_vi, delta_si = Update(v_j, s_j)
+            v_j = delta_vi + v_j
+            s_j = delta_si + s_j
+        
+        # Final reduction block
+
+        self.final_reduction = nn.Sequential(
+            nn.Linear(self.num_features, self.num_features),
+            nn.SiLU(),
+            nn.Linear(self.num_features, self.num_features)
+        )
+
+        output = self.final_reduction(s_j)
+
+        energy = output.sum()
+
+        print("Hello")
+    
+    def make_adjecency_matrix(self, atom_positions, graph_indexes):
+
+        N = len(graph_indexes)
+        A_col = []
+        A_row = []
+
+        indices = torch.arange(N).cuda()
+
+        for i, gi in enumerate(graph_indexes):
+            matching_graph_index = graph_indexes == gi
+            matching_graph_index[i] = False   # Remove the current index from list (to avoid loops)
+            pos_same_molecule = atom_positions[matching_graph_index]
+            dist_mask = torch.norm(pos_same_molecule - atom_positions[i], 
+                                   p=2, dim=1) < self.cutoff_dist
+
+            A_row.append(torch.Tensor.tile(torch.IntTensor([i]), int(dist_mask.sum()) ).cuda())
+            A_col.append(indices[matching_graph_index][dist_mask])
+
+        A_row = torch.cat(A_row)
+        A_col = torch.cat(A_col)
+
+        return A_row, A_col
+
 
 class Message():   
     def __init__(self):
         pass
 
     def foward(self):
+
         # For computing phi
         self.sj_linear = nn.Sequential(
             nn.Linear(self.num_features, self.num_features),
@@ -95,14 +159,14 @@ class Message():
        
         return delta_sim, delta_vim
     
-    def update():
+class Update():
         raise NotImplementedError
     
-    def gated_equivariant_block():
-        raise NotImplementedError
+    # def gated_equivariant_block():
+    #     raise NotImplementedError
     
-    def radial_basis_function():
-        raise NotImplementedError
+    # def radial_basis_function():
+    #     raise NotImplementedError
     
-    def cosine_cutoff():
-        raise NotImplementedError
+    # def cosine_cutoff():
+    #     raise NotImplementedError
